b7ca2e5c60d9cdde43a7d5f78eefcd6727bbc0fc
Kirill <qSatan1s@yandex.ru>
2021-01-04 01:28:33 +0300

    1

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+
+@Module({
+  imports: [GraphQLModule.forRoot({
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),   
+  })],
+
+})
+export class AppModule {}
