fdeabe28f9f33ccd5edc2ae9f52805b2a648c417
Kirill <qSatan1s@yandex.ru>
2021-01-04 22:37:37 +0300

    graphql + postgresql

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,17 +1,29 @@
-import { ObjectType, Field } from '@nestjs/graphql';
-
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
-  @Field(type => String)
-  name: string;
-  @Field(type => Boolean)
-  isVegan ?: boolean
+  @PrimaryGeneratedColumn()
+  @Field((type) => Number)
+  id: number;
 
-  @Field(type => String)
+  @Field((type) => String)
+  @Column()
+  name: string;
+  @Field((type) => Boolean)
+  @Column()
+  isVegan: boolean;
+
+  @Field((type) => String)
+  @Column()
   address: string;
 
-  @Field(type => String)
+  @Field((type) => String)
+  @Column()
   ownerName: string;
 
+  @Field((type) => String)
+  @Column()
+  categoryName: string;
 }
