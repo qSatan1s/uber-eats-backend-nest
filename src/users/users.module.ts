0756f31d2af50941dafa8e91428720488bf46c35
Kirill <qSatan1s@yandex.ru>
2021-02-01 07:19:35 +0300

    JWT Module part Two

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
-import { JwtService } from '../jwt/jwt.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), ConfigService, JwtService],
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersResolver, UsersService],
 })
 export class UsersModule {}
